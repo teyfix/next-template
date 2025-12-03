da1f10873349a4a38397c032b4962cfc59d0e64e
Halil Teyfik <hlltvfk@gmail.com>
2025-12-03 19:45:53 +0300

    feat(docker): add docker support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const TRAEFIK_DOMAIN = process.env.TRAEFIK_DOMAIN;
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
 };
+
+if (TRAEFIK_DOMAIN) {
+  nextConfig.allowedDevOrigins = [`https://${TRAEFIK_DOMAIN}`];
+}
 
 export default nextConfig;
