179d0b5580005e223e06b5f24d2d3eb9d06ada76
Halil Teyfik <hlltvfk@gmail.com>
2025-12-20 04:38:24 +0300

    fix(config/helpers): envArray returns [""] for empty values

--- src/config/shared/helpers.ts
+++ src/config/shared/helpers.ts
@@ -1,8 +1,16 @@
 import z, { type ZodObject } from "zod";
 import type { ConfigModel } from "./types";
 
-export const envArray = () =>
-  z.string().transform((val) => val.split(";").map((e) => e.trim()));
+export const envArray = (): z.ZodPipe<
+  z.ZodString,
+  z.ZodTransform<string[], string>
+> =>
+  z.string().transform((val) =>
+    val
+      .split(";")
+      .map((e) => e.trim())
+      .filter((e) => e !== ""),
+  );
 
 export const envLoader = <S extends ZodObject>(
   schema: S,
